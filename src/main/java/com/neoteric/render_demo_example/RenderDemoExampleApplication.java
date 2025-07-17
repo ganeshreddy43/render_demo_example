b21882fe8434d228f71dabf66d0dec78fb04b697
ganeshreddy43 <reddyganesh2677@gmail.com>
2025-07-17 23:52:43 +0530

    Initial commit with HTML and CSS files

---
+++ src/main/java/com/neoteric/render_demo_example/RenderDemoExampleApplication.java
@@ -0,0 +1,13 @@
+package com.neoteric.render_demo_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenderDemoExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenderDemoExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neoteric/render_demo_example/RenderDemoExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neoteric.render_demo_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenderDemoExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
